79d7ab725823c52564f0b17d279a5ac3bc51ace0
BeeIsStr <156618675+esthersinche@users.noreply.github.com>
2025-05-26 22:20:20 -0500

    Initial commit

---
+++ src/main/java/com/utp/pizzatime/PizzaTime.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utp.pizzatime;
+
+/**
+ *
+ * @author BeeIsMega
+ */
+public class PizzaTime {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
